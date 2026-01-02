1a2a79b99d0bfda0a3933c28a213880e7a33b7e8
siby369 <sibyraj369@gmail.com>
2026-01-02 11:20:00 +0530

    develop github stars visualization demonstration

---
+++ src/registry/examples/github-stars-demo.tsx
@@ -0,0 +1,5 @@
+import { GitHubStars } from "@/registry/components/github-stars"
+
+export default function GitHubStarsDemo() {
+  return <GitHubStars repo="siby369/Siby.com" stargazersCount={1050} />
+}
